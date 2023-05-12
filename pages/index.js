b65d7a8d89dc6f7b582a5f13fe1a7e83c5b9ce89
André Luiz Schilling <84943896+sigvum@users.noreply.github.com>
2023-05-12 13:00:10 +0000

    saída do codespaces

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+function Home() {
+    return ([
+        <div>
+        <img src='https://mega.ibxk.com.br//2014/2/megacurioso/141943139004085840.jpg'>
+        </div>
+        ]);
+}
+
+export default Home;

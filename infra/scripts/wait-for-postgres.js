b3a0ba1fe567a57b860250e9749227c95f8668ef
sigvum <tsemanoa@cryptolab.net>
2024-09-20 16:48:14 -0300

    adds `wait-for-postgres,js` script

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,19 @@
+const { exec } = require("node:child_process")
+
+function checkPostgres() {
+    exec('docker exec postgres-dev pg_isready --host localhost', handReturn);
+
+    function handReturn(error, stdout) {
+        if (stdout.search("accepting connections") === -1) {
+            process.stdout.write(".");
+            checkPostgres();
+            return;
+        }
+
+        console.log("\n🏁 Postgres está pronto e aceitando conexões!\n")
+    }
+}
+
+
+process.stdout.write("\n\n🚩 Aguardando Postgres aceitar conexões");
+checkPostgres();

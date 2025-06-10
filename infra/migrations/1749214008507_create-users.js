c3e22f4be700485736d284b3851bfc2254e2e8b5
sigvum <tsemanoa@cryptolab.net>
2025-06-10 10:33:09 -0300

    feat: cria user migration x

--- infra/migrations/1749214008507_create-users.js
+++ infra/migrations/1749214008507_create-users.js
@@ -20,21 +20,23 @@ exports.up = (pgm) => {
       unique: true,
     },
 
-    // Why 72 in leght? https://security.stackexchange.com/a/39851
+    // Why 60 in leght? https://www.npmjs.com/package/bcrypt#hash-info
     password: {
-      type: "varchar(72)",
+      type: "varchar(60)",
       notNull: true,
     },
 
     // Why timezone? https://justatheory.com/2012/04/postgres-use-timestamptz/
     created_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
     },
 
     updated_at: {
       type: "timestamptz",
-      default: pgm.func("now()"),
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
     },
   });
 };
